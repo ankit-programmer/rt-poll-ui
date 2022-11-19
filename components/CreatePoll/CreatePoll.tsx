d616e6798dc09beffe1f3b70e7e0637df91d65fe
Ankit Kumar <ankitkumar@whozzat.com>
2022-11-19 23:23:51 +0530

    Question design improvement

--- components/CreatePoll/CreatePoll.tsx
+++ components/CreatePoll/CreatePoll.tsx
@@ -1,26 +1,58 @@
-import React from 'react'
-import  styles from './CreatePoll.module.css';
+import React, { useEffect, useState } from 'react'
+import styles from './CreatePoll.module.css';
 const CreatePoll = () => {
+  let placeholders = ["Who will you vote for?", "Who will win this T20 World Cup?", "Where should we go for vocation?"];
+  let [placeholder, setPlaceholder] = useState("");
+  useEffect(() => {
+    (async () => {
+      while(placeholders.length){
+        let ph = placeholders.shift();
+        placeholders.push(ph || "");
+        let sent = "";
+        let words = ph?.split(" ") || [];
+        for(const word of words){
+          sent += `${word} `;
+          setPlaceholder(sent);
+          await dummyWait(300);
+        }
+        await dummyWait(5000);
+       
+      }
+     
+    })();
+  }, [])
   return (
-   <div className='w-full h-screen text-center'>
-    <div className='max-w-[1240px] w-full h-full mx-auto p-5 flex justify-center items-center'>
+    <div className='w-full h-screen text-center'>
+      <div className='max-w-[1240px] w-full h-full mx-auto p-5 flex justify-center items-center'>
 
-        <form>
-          <input  className={styles.QuestionInput} type="text" id='question' placeholder='Who will you vote for?'></input>
-         <div>
-         <input  className={styles.OptionInput} type="text" id='question' placeholder='Option One'></input>
-          <input  className={styles.OptionInput} type="text" id='question' placeholder='Option Two'></input>
-          <input  className={styles.OptionInput} type="text" id='question' placeholder='Option Three'></input>
-          <input  className={styles.OptionInput} type="text" id='question' placeholder='Option Four'></input>
-        
-         </div>
-       
-         </form>
-       
+        <form className={styles.PollContainer}>
+      
+
+          <div className={styles.QuestionIcon}>Q</div>
+          
+          <input className={styles.QuestionInput} type="text" id='question' placeholder={placeholder}></input>
+      
+          <div className={styles.OptionContainer}>
+            <input className={styles.OptionInput} type="text" id='question' placeholder='Option One'></input>
+            <input className={styles.OptionInput} type="text" id='question' placeholder='Option Two'></input>
+            <input className={styles.OptionInput} type="text" id='question' placeholder='Option Three'></input>
+            <input className={styles.OptionInput} type="text" id='question' placeholder='Option Four'></input>
+
+          </div>
+
+        </form>
+
+      </div>
     </div>
-   </div>
-    
+
   )
 }
 
+function dummyWait(time = 1000) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return resolve(true);
+    }, time);
+  })
+}
 export default CreatePoll
