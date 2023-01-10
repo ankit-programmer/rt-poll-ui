c3eb50002523f08d5ac9f95160e855bc365a7382
Ankit Kumar <ankitkumar@whozzat.com>
2023-01-10 21:53:49 +0530

    Added support for anonymous login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,14 @@ import '../styles/globals.css';
 import '../styles/design_tokens.css';
 import { Provider } from 'react-redux';
 import { store } from '../app/store';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged, signInAnonymously } from 'firebase/auth';
 import { auth } from '../app/firebaseApp'
 
 
 export default function App({ Component, pageProps }: AppProps) {
+  if (!auth.currentUser) {
+    signInAnonymously(auth);
+  }
   onAuthStateChanged(auth, (user: any) => {
     console.log(user);
   })
