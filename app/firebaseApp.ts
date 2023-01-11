fa12f4481cbaaa1f578abfd9cb1dc18155d39f39
Ankit Kumar <ankitkumar@whozzat.com>
2023-01-11 12:32:22 +0530

    Fixed autoUpgradeAnonymousUser issue

--- app/firebaseApp.ts
+++ app/firebaseApp.ts
@@ -1,6 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, onAuthStateChanged, EmailAuthProvider } from 'firebase/auth';
-initializeApp({
+import firebase from 'firebase/compat/app';
+import "firebase/compat/auth";
+
+firebase.initializeApp({
     apiKey: "AIzaSyCLHjoSpPueDt3_Fy5BcyT9vKWLXuh0sjw",
     authDomain: "rt-poll.firebaseapp.com",
     projectId: "rt-poll",
@@ -8,5 +11,14 @@ initializeApp({
     messagingSenderId: "146578956832",
     appId: "1:146578956832:web:c7bdf3b5046a0674e39245",
     measurementId: "G-TJ180W7MZG"
-});
-export const auth = getAuth();
+})
+// initializeApp({
+//     apiKey: "AIzaSyCLHjoSpPueDt3_Fy5BcyT9vKWLXuh0sjw",
+//     authDomain: "rt-poll.firebaseapp.com",
+//     projectId: "rt-poll",
+//     storageBucket: "rt-poll.appspot.com",
+//     messagingSenderId: "146578956832",
+//     appId: "1:146578956832:web:c7bdf3b5046a0674e39245",
+//     measurementId: "G-TJ180W7MZG"
+// });
+export const auth = firebase.auth();
