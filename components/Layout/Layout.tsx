d59989173eb2c10a8377fcbd9fee41c9d70c8068
Ankit Kumar <ankitkumar@whozzat.com>
2023-01-31 02:40:39 +0530

    Lazy loading nav bar

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,9 +1,10 @@
+import dynamic from 'next/dynamic';
 import { useSelector } from 'react-redux';
-import NavBar from '../NavBar/NavBar';
+// import NavBar from '../NavBar/NavBar';
 export default function Layout({ children }: any) {
-    // const NavBar = dynamic(() => import('../NavBar/NavBar'), {
-    //     loading: () => null
-    // })
+    const NavBar = dynamic(() => import('../NavBar/NavBar'), {
+        loading: () => null
+    })
     return (
         <>
             <NavBar></NavBar>
