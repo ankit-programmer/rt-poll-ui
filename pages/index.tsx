f9bacceb45303ca9db9efdadc7ffcefdb46a5f27
Ankit Kumar <ankitkumar@whozzat.com>
2023-02-06 02:25:59 +0530

    Changed Title of Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Poll | Get Your Friends/Team Opinion in seconds!</title>
+        <title>Create Free Poll | Ad Free | RT Poll</title>
         <meta name="description" content="Are you tired of long and tedious meetings just to reach a decision? Introduce RTPoll to your team! Our real-time online poll app makes decision-making easy and efficient. With RTPoll, you can create polls with multiple choice questions, invite your team members to participate, and view results in real-time. No more waiting for responses or trying to schedule meetings that work for everyone. With RTPoll, your team will be more engaged and decisions will be made faster. Visit our website now to learn more and start using RTPoll today!" />
         <meta name='keywords' content='Real-time poll, Team decision making, Online poll app, Free poll app, Ad-free poll app, Image poll, Intuitive UI poll, Social media sharing, Poll result graph, Group decision making app, Team opinion poll, Online survey tool, Easy to use poll app, Decision making tool, Team engagement, Group opinion poll, Free survey app, Real-time survey'></meta>
         <link rel="icon" href="/favicon.png" />
